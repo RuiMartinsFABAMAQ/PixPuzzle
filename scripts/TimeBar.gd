798856fdf5b149df084828c72cb9739d137dade2
Rui Martins <rui.martins@fabamaq.com>
2022-02-13 22:34:33 +0000

    game_version2.0

--- scripts/TimeBar.gd
+++ scripts/TimeBar.gd
@@ -3,13 +3,14 @@ extends Line2D
 signal time_out;
 
 func _decrease():
-	points[1].x -= 11.5;
+	points[1].x -= 10;
 
 func no_more_time():
 	return points[1].x ==  0;
 
 func _on_Timer_timeout():
-	_decrease();
 	if no_more_time():
 		emit_signal("time_out");
-		
+		return
+	_decrease();
+	
