b980ef997e8c156c7ccefa06f29a5ad70d78c3fa
Rui Martins <rui.martins@fabamaq.com>
2022-02-24 13:14:33 +0000

    update

--- scripts/Root.gd
+++ scripts/Root.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var anim = $easyin
+
 var currentPiece = null
 var game_over = false
 
@@ -8,11 +10,11 @@ func _unhandled_input(event):
 		get_tree().reload_current_scene()
 
 func _ready():
-	$easyin.play("fade_out")
+	anim.play("fade_out")
 	var timeout = get_tree().get_root().find_node("Time_bar", true, false)
 	timeout.connect("game_over",self, "game_over")
 	
 func game_over():
-	$easyin.play("fade_in")
+	anim.play("fade_in")
 	yield(get_tree().create_timer(0.7), "timeout")
 	get_tree().change_scene("res://scenes/gameover_scene.tscn")
--- scripts/Welcome.gd
+++ scripts/Welcome.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
+onready var anim = $easyin3
+
 func _on_start_pressed():
-	$easyin3.play("fade_in")
+	anim.play("fade_in")
 	yield(get_tree().create_timer(0.5), "timeout")
 	get_tree().change_scene("res://scenes/level1.tscn")
